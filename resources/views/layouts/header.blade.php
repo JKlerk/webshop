e2ced73bfe2e633532dc7970a31e315e0b7a9457
JKlerk <99045878@mydavinci.nl>
2019-05-26 00:15:49 +0200

    Changes header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,6 +2,7 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
+
 <div id="app">
 	
 	<div class="p-5 bg-white w-full shadow text-center">
@@ -10,11 +11,11 @@
 	</div>
 
     <nav class="bg-black p-3 shadow flex mb-8">
-        <div class="p-1 flex justify-center w-full">
-            <a class="hover:text-grey mx-3 text-white text-base" href="{{ url('/') }}">Home</a>
-            <a class="hover:text-grey mx-3 text-white text-base" href="{{ url('/') }}">Example 1</a>
-            <a class="hover:text-grey mx-3 text-white text-base" href="{{ url('/') }}">Example 2</a>
-            <a class="hover:text-grey mx-3 text-white text-base" href="{{ url('/') }}">Contact</a>
+        <div class="p-1 flex justify-end w-full">
+            <div class="w-full my-auto flex justify-end mr-3">
+                <a class="text-white no-underline hover:underline" href="/login">Login/Register</a>
+            </div>
+            <a class="bg-green rounded text-white p-3 no-underline" href=""><i class="fas fa-shopping-cart"></i></a>
         </div>
     </nav>
 </div>
