8b346bf3d8c509282d296d680312214e6f631891
JKlerk <99045878@mydavinci.nl>
2019-05-29 10:09:56 +0200

    Admin panel

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,10 +7,12 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function index() {
-    	$products = App\Product::all();
+    	// $products = App\Product::all();
 
     	if (auth()->user()->level == 1){
-	    	return view('pages.index', compact('products'));
+	    	return view('admin.index');
+    	} else{
+    		return redirect()->action('PageController@index');
     	}
 
     }
