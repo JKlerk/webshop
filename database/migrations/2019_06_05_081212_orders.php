5ff4e3f9c151c1316b2940fcf571e93434260c30
JKlerk <99045878@mydavinci.nl>
2019-06-05 10:25:57 +0200

    Migration changes

--- database/migrations/2019_06_05_081212_orders.php
+++ database/migrations/2019_06_05_081212_orders.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Products extends Migration
+class Orders extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class Products extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('src')->default('placeholder.png');
-            $table->string('title');
-            $table->string('desc');
-            $table->string('status')->default('In Stock');
-            $table->string('size')->nullable();
-            $table->string('price');
+            $table->string('product_id')
+            $table->string('user_id');
+            $table->string('status');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class Products extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('products');
+        //
     }
 }
