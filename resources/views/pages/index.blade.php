0189fcd9af4e18b42391af2a594026f95414639a
JKlerk <99045878@mydavinci.nl>
2019-06-07 10:21:34 +0200

    Change layout

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -11,23 +11,18 @@
 		<div class="flex justify-center">
 			<div class="shadow w-full h-px my-2 bg-grey-darkest"></div>
 		</div>
-		<div class="flex justify-center">
-			<div class="w-full flex">
-				@foreach($products as $product)
-					<div class="bg-grey-lightest w-full mx-2 p-5">
-						<h1>{{$product->title}}</h1>
-						<h2 class="text-grey mb-10">{{$product->shortdesc}}</h2>
-						<p>{{$product->desc}}</p>
+		<div class="flex flex-wrap justify-center">
+			@foreach($products as $product)
+				<div class="border border-grey-light mx-1 p-5 mb-5" style="width: 49%">
+					<h1>{{$product->title}}</h1>
+					<h2 class="text-grey mb-10">{{$product->shortdesc}}</h2>
+					<p>{{$product->desc}}</p>
 
-						<div class="w-full mt-5 flex justify-end">
-							<button class="p-2 border-black border-2  bg-white no-underline hover:shadow"><a class="hover:underline text-black" href="{{ url('view/' . $product->id) }}">Read more</a></button>
-						</div>
+					<div class="w-full mt-5 flex justify-end">
+						<button class="p-2 border-black border-2  bg-white no-underline hover:shadow"><a class="hover:underline text-black" href="{{ url('view/' . $product->id) }}">Read more</a></button>
 					</div>
-				@endforeach
-				<div class="bg-grey-lightest w-full mx-2">
-					<h1>ss</h1>
 				</div>
-			</div>
+			@endforeach
 		</div>
 	</div>
 
