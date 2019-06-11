c2eae5a14dc0d62949f9bd9e4ccbf551dc8455e5
JKlerk <99045878@mydavinci.nl>
2019-06-11 14:51:53 +0200

    admin lvl change

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -17,16 +17,12 @@ class PageController extends Controller
     {
         return view('pages.profile');
     }
-    
+
     public function admin() {
 
     	$products = App\Product::all();
 
-    	if (auth()->user()->level == 1){
-	    	return view('pages.admin', compact('products'));
-    	} else{
-    		return redirect()->action('PageController@index');
-    	}
+    	return view('pages.admin', compact('products'));
 
     }
 }
